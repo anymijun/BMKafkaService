28a84333fafdb81bbde5da70304f2a69aa89d371
LeonWong <768357432@qq.com>
2016-07-18 09:28:12 +0800

    将Kafka配置信息写入application.yml

--- KafkaService/src/main/java/com/bluemoon/kafka/config/KafkaProducerFactory.java
+++ KafkaService/src/main/java/com/bluemoon/kafka/config/KafkaProducerFactory.java
@@ -2,44 +2,80 @@ package com.bluemoon.kafka.config;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.Assert;
 
+import java.util.List;
 import java.util.Properties;
 
 /**
- * 考虑一个系统多个Topic的封装场景
- *
+ * Kafka生产者工厂
+ * 
  * Created by LeonWong on 16/7/15.
  */
 @Configuration
+@ConfigurationProperties(prefix = "kafka")
 public class KafkaProducerFactory {
 
-    /**
-     * 默认TOPIC
-     *
-     * @TODO 打算写入配置
-     */
-    private static final String KAFKA_CLUSTER = "127.0.0.1:9092";
+    private List<String> clusters;
+
+    private Integer batchsize;
+
+    private Integer buffermemory;
+
+    private Integer retries = 0;
 
     @Bean(name = "mqProducer")
     public Producer getInstance() {
+        Assert.notNull(clusters);
         /**
          * 参考文档
          *
          * @URL http://kafka.apache.org/documentation.html#configuration
          */
         Properties props = new Properties();
-        props.put("bootstrap.servers", KAFKA_CLUSTER);
+        props.put("bootstrap.servers", clusters);
         props.put("acks", "all");
-        props.put("retries", 0);
-        props.put("batch.size", 16384);
+        props.put("retries", retries);
+        props.put("batch.size", batchsize);
         props.put("linger.ms", 1);
-        props.put("buffer.memory", 33554432);
+        props.put("buffer.memory", buffermemory);
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        Producer<String, Object> producer = new KafkaProducer<>(props);
-        return producer;
+        return new KafkaProducer<>(props);
     }
 
+    public List<String> getClusters() {
+        return clusters;
+    }
+
+    public void setClusters(List<String> clusters) {
+        this.clusters = clusters;
+    }
+
+    public Integer getBuffermemory() {
+        return buffermemory;
+    }
+
+    public void setBuffermemory(Integer buffermemory) {
+        this.buffermemory = buffermemory;
+    }
+
+    public Integer getBatchsize() {
+        return batchsize;
+    }
+
+    public void setBatchsize(Integer batchsize) {
+        this.batchsize = batchsize;
+    }
+
+    public Integer getRetries() {
+        return retries;
+    }
+
+    public void setRetries(Integer retries) {
+        this.retries = retries;
+    }
 }
